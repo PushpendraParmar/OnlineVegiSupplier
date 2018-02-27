79d3710a96d827f513dc791cfe028654f7cfe5a0
Pushpendra Parmar <pushpendra.parmar217@gmail.com>
2018-02-27 22:34:37 +0530

    Revert "Revert "Product list by mocdata""
    This reverts commit d9b3efa70f5e70f5d5ef1d25f01b84fc35a10853.

---
+++ src/app/MocData/moc-products.ts
@@ -0,0 +1,20 @@
+import { ProductModel } from '../Model/ProductModel';
+
+
+
+export const PRODTCTS: ProductModel[] = [
+    {productName:"Ipsums Dolors Untra",imagePath:"img12.jpg", productPrice:188.80, oldProductPrice:299 },
+    {productName:"Ipsums Dolors Untra",imagePath:"img13.jpg", productPrice:188.80, oldProductPrice:299 },
+    {productName:"Ipsums Dolors Untra",imagePath:"img14.jpg", productPrice:188.80, oldProductPrice:299 },
+    {productName:"Ipsums Dolors Untra",imagePath:"img15.jpg", productPrice:188.80, oldProductPrice:299 },
+    {productName:"Ipsums Dolors Untra",imagePath:"img16.jpg", productPrice:188.80, oldProductPrice:299 },
+    {productName:"Ipsums Dolors Untra",imagePath:"img17.jpg", productPrice:188.80, oldProductPrice:299 },
+    {productName:"Ipsums Dolors Untra",imagePath:"img18.jpg", productPrice:188.80, oldProductPrice:299 },
+    {productName:"Ipsums Dolors Untra",imagePath:"img19.jpg", productPrice:188.80, oldProductPrice:299 },
+    {productName:"Ipsums Dolors Untra",imagePath:"img20.jpg", productPrice:188.80, oldProductPrice:299 },
+    {productName:"Ipsums Dolors Untra",imagePath:"img21.jpg", productPrice:188.80, oldProductPrice:299 },
+    {productName:"Ipsums Dolors Untra",imagePath:"img22.jpg", productPrice:188.80, oldProductPrice:299 },
+    {productName:"Ipsums Dolors Untra",imagePath:"img23.jpg", productPrice:188.80, oldProductPrice:299 },
+    {productName:"Ipsums Dolors Untra",imagePath:"img24.jpg", productPrice:188.80, oldProductPrice:299 },
+    {productName:"Ipsums Dolors Untra",imagePath:"img02.jpg", productPrice:188.80, oldProductPrice:299 }
+];
