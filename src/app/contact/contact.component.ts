ccfb02e16190a14476a04ff440588c2bfbc6e761
Narendra Singh <narendrasikarwar04@gmail.com>
2018-03-18 14:16:47 +0530

    Submit values from Enquiry From

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -11,5 +12,10 @@ export class ContactComponent implements OnInit {
 
   ngOnInit() {
   }
+  saveEnquiry(enquiryForm: NgForm): void{
+    console.log(enquiryForm.value);
+  }
+
+  
 
 }
